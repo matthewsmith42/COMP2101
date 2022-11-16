0d076e335b0975b5832f318686db1f788fec5f82
Matthew Smith <mr.mgsmith17@gmail.com>
2022-11-16 13:19:29 -0500

    Changing format list to format table

--- powershell/ipconfigreport.ps1
+++ powershell/ipconfigreport.ps1
@@ -2,5 +2,5 @@
 # This script creates a report that shows the IP configuration and other details of all enabled adapters for the computer
 
 Get-CimInstance Win32_NetworkAdapterConfiguration | Where-Object IPEnabled -EQ True | 
-Format-List -Property Description, Index, IPAddress, IPSubnet, DNSDomain, DNSServerSearchOrder
+Format-Table -Property Description, Index, IPAddress, IPSubnet, DNSDomain, DNSServerSearchOrder
 
